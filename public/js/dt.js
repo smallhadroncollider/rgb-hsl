e0f793838753a49afa4a148693b8bb90bfb0b975
Mark Wales <mark@smallhadroncollider.com>
2015-01-25 10:52:15 +0000

    added dt module

---
+++ public/js/dt.js
@@ -0,0 +1,57 @@
+(function () {
+    "use strict";
+
+    var R = require("../vendor/ramda/ramda");
+    var scene = require("./scene");
+
+    var dt = function () {
+        var self = {},
+            id = 0,
+            funcs = {},
+            time = 0,
+            loop = function (now) {
+                requestAnimationFrame(loop);
+                var values = R.values(funcs);
+
+                if (values.length) {
+                    R.forEach(function (func) {
+                        func(now - time);
+                    }, values);
+
+                    scene.render();
+                }
+
+                time = now;
+            };
+
+        loop(time);
+
+        self.add = function (func) {
+            id++;
+            funcs[id] = func;
+            return id;
+        };
+
+        self.remove = function (id) {
+            delete(funcs[id]);
+        };
+
+        self.animate = function (object, property, newValue, length) {
+            var id = self.add(function (dt) {
+                if (length <= 0) {
+                    self.remove(id);
+                    object[property] = newValue;
+                }
+
+                var diff = newValue - object[property];
+                object[property] += diff / (length/dt);
+
+                length = length - dt;
+            });
+        };
+
+        return self;
+    };
+
+    module.exports = dt();
+}());
