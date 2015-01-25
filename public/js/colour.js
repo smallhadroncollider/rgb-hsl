56b7f03c95cf0e49a4b64c11f12fa59ec50fc4b4
Mark Wales <mark@smallhadroncollider.com>
2015-01-25 13:50:02 +0000

    fixed rounding bugs in colour

--- public/js/colour.js
+++ public/js/colour.js
@@ -3,34 +3,46 @@
 
     var R = require("../vendor/ramda/ramda");
 
-    module.exports = {
-        hsl: function (r, g, b) {
-            r = r / 255;
-            g = g / 255;
-            b = b / 255;
+    var normalise = R.curry(function (min, max, val) {
+        if (val > max) {
+            return max;
+        } else if (val < min) {
+            return min;
+        }
 
-            var rgb = [r, g, b];
+        return val;
+    });
+
+    var normaliseSL = normalise(0, 1);
+
+    module.exports = {
+        hsl: function (red, green, blue) {
+            red = red / 255;
+            green = green / 255;
+            blue = blue / 255;
+
+            var rgb = [red, green, blue];
             var cMax = R.max(rgb);
             var cMin = R.min(rgb);
-            var c = cMax - cMin;
+            var chroma = cMax - cMin;
 
-            var l = (cMax + cMin) / 2;
-            var s = c === 0 ? 0 : (c / (1 - Math.abs((2 * l) - 1)));
-            var h;
+            var lightness = normaliseSL((cMax + cMin) / 2);
+            var saturation = normaliseSL(chroma === 0 ? 0 : (chroma / (1 - Math.abs((2 * lightness) - 1))));
+            var hue;
 
-            if (c) {
-                if (r >= g && r >= b) {
-                    h = 60 * (((g - b)/c) % 6);
-                } else if (g >= r && g >= b) {
-                    h = 60 * (((b - r)/c) + 2);
-                } else if (b >= r && b >= g) {
-                    h = 60 * (((r - g)/c) + 4);
+            if (chroma) {
+                if (red >= green && red >= blue) {
+                    hue = 60 * (((green - blue)/chroma) % 6);
+                } else if (green >= red && green >= blue) {
+                    hue = 60 * (((blue - red)/chroma) + 2);
+                } else if (blue >= red && blue >= green) {
+                    hue = 60 * (((red - green)/chroma) + 4);
                 }
             } else {
-                h = 0;
+                hue = 0;
             }
 
-            return [h, s, l];
+            return [hue, saturation, lightness];
         }
     };
 }());
