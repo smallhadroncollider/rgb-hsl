8010bef3b416539949f9e8ea59b6dd9a22c121a0
Mark Wales <mark@smallhadroncollider.com>
2015-01-25 09:55:23 +0000

    moved js into separate file

--- make/watch.make
+++ make/watch.make
@@ -1,24 +1,25 @@
 .PHONY: watch
 
-watch: $(refs)/index.ref
+watch: $(refs)/index.ref $(dev_public)/app.js
 
 $(refs)/index.ref: public/index.html
 	@ touch $@
 	@ chrome-canary-cli reload
+
+$(dev_public)/app.js: $(dev_js)
+	@ browserify $(dev_public)/js/main.js > $@
+	@ chrome-canary-cli reload
+	# @ check=""; \
+	# 	for i in $?; do \
+	# 		if [ $$(printf "%s" $$i | tail -c 3) = ".js" ]; then \
+	# 			check="$$check $$i"; \
+	# 		fi \
+	# 	done; \
+	# 	jshint $$check; \
+	# 	buddy $$check;
 
 $(dev_css): $(dev_scss)
 	@ compass compile && terminal-notifier -message "Compass Compiled" -title "Compass" -group temp && sleep 1 && terminal-notifier -remove temp
 	@ touch $@
 	@ chrome-canary-cli reload
 
-$(refs)/jshint: $(dev_js) $(dev_js_templates)
-	@ touch $@
-	@ chrome-canary-cli reload
-	@ check=""; \
-		for i in $?; do \
-			if [ $$(printf "%s" $$i | tail -c 3) = ".js" ]; then \
-				check="$$check $$i"; \
-			fi \
-		done; \
-		jshint $$check; \
-		buddy $$check;
---
+++ public/js/main.js
@@ -0,0 +1,209 @@
+(function (d) {
+    "use strict";
+
+    var R = require("../vendor/ramda/ramda");
+    var THREE = require("../vendor/threejs/build/three");
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(1000, 600);
+    renderer.setClearColor(0x808080, 1);
+    document.body.appendChild(renderer.domElement);
+
+    var number = 12;
+    var factor = 255 / number;
+
+    var range = R.range(0, number);
+
+    var size = 2;
+    var gap = 2;
+    var offset = number * ((size + gap) / 2);
+
+    var hsl = function (r, g, b) {
+        r = r / 255;
+        g = g / 255;
+        b = b / 255;
+
+        var rgb = [r, g, b];
+        var cMax = R.max(rgb);
+        var cMin = R.min(rgb);
+        var c = cMax - cMin;
+
+        var l = (cMax + cMin) / 2;
+        var s = c === 0 ? 0 : (c / (1 - Math.abs((2 * l) - 1)));
+        var h;
+
+        if (c) {
+            if (r >= g && r >= b) {
+                h = 60 * (((g - b)/c) % 6);
+            } else if (g >= r && g >= b) {
+                h = 60 * (((b - r)/c) + 2);
+            } else if (b >= r && b >= g) {
+                h = 60 * (((r - g)/c) + 4);
+            }
+        } else {
+            h = 0;
+        }
+
+        return [h, s, l];
+    };
+
+    var position = function (val) {
+        return (val * (size + gap)) - offset;
+    };
+
+    var value = function (x) {
+        return Math.round(x * factor);
+    };
+
+    var create = function (r, g, b) {
+        var rgb = R.map(value, [r, g, b]);
+
+        var color = "rgb(" + rgb.join(",") + ")";
+        var geometry = new THREE.BoxGeometry(size, size, size);
+        var material = new THREE.MeshBasicMaterial({ color: color, transparent: true, opacity: 1 });
+
+        r = position(r);
+        g = position(g);
+        b = position(b);
+
+        var cube = new THREE.Mesh(geometry, material);
+
+        cube.userData.rgb = [r, g, b];
+        cube.userData.hsl = hsl.apply(null, rgb);
+
+        return cube;
+    };
+
+    var lift = R.liftN(3, R.curryN(3, create));
+    var cubes = lift(range, range, range);
+
+    var group = new THREE.Group();
+    group.add.apply(group, cubes);
+    scene.add(group);
+
+    var dt = (function () {
+        var self = {},
+            id = 0,
+            funcs = {},
+            time = 0,
+            loop = function (now) {
+                requestAnimationFrame(loop);
+                var values = R.values(funcs);
+
+                if (values.length) {
+                    R.forEach(function (func) {
+                        func(now - time);
+                    }, values);
+
+                    renderer.render(scene, camera);
+                }
+
+                time = now;
+            };
+
+        loop(time);
+
+        self.add = function (func) {
+            id++;
+            funcs[id] = func;
+            return id;
+        };
+
+        self.remove = function (id) {
+            delete(funcs[id]);
+        };
+
+        self.animate = function (object, property, newValue, length) {
+            var id = self.add(function (dt) {
+                if (length <= 0) {
+                    self.remove(id);
+                    object[property] = newValue;
+                }
+
+                var diff = newValue - object[property];
+                object[property] += diff / (length/dt);
+
+                length = length - dt;
+            });
+        };
+
+        return self;
+    }());
+
+    dt.add(function () {});
+
+    var toRGB = function () {
+        R.forEach(function (cube) {
+            dt.animate(cube.position, "x", cube.userData.rgb[0], 5000);
+            dt.animate(cube.position, "y", cube.userData.rgb[1], 5000);
+            dt.animate(cube.position, "z", cube.userData.rgb[2], 5000);
+        }, cubes);
+    };
+
+    var toHSL = function () {
+        R.forEach(function (cube) {
+            var x = (cube.userData.hsl[2] * number * size * gap * 2) - offset * 1.5;
+
+            var r = cube.userData.hsl[1] * number * 2;
+            var theta = ((cube.userData.hsl[0] / 360) * 2 * Math.PI) + (1.2 * Math.PI);
+            var y = r * Math.cos(theta);
+            var z = r * Math.sin(theta);
+
+            dt.animate(cube.position, "x", x, 5000);
+            dt.animate(cube.position, "y", y, 5000);
+            dt.animate(cube.position, "z", z, 5000);
+        }, cubes);
+    };
+
+    var toHSLCube = function () {
+        R.forEach(function (cube) {
+            dt.animate(cube.position, "x", position((cube.userData.hsl[0] / 360) * factor), 5000);
+            dt.animate(cube.position, "y", position(cube.userData.hsl[1] * factor), 5000);
+            dt.animate(cube.position, "z", position(cube.userData.hsl[2] * factor), 5000);
+        }, cubes);
+    };
+
+    var saturation = function (val) {
+        R.forEach(function (cube) {
+            if (cube.userData.hsl[1] > val) {
+                dt.animate(cube.material, "opacity", 1, 1000);
+            } else {
+                dt.animate(cube.material, "opacity", 0.1, 1000);
+            }
+        }, cubes);
+    };
+
+    document.getElementById("hsl").onclick = toHSL;
+    document.getElementById("rgb").onclick = toRGB;
+
+    var saturationSlider = document.getElementById("saturation");
+
+    saturationSlider.onchange = function () {
+        saturation(saturationSlider.value);
+        document.getElementById("saturation-value").innerHTML = saturationSlider.value;
+    };
+
+
+    camera.position.z = number * 6;
+
+    toRGB();
+    renderer.render(scene, camera);
+
+    var rotate = 0.1 / Math.PI;
+
+    window.onkeydown = function (e) {
+        switch (e.keyCode) {
+            case 37: group.rotation.y -= rotate; e.preventDefault(); break; // right
+            case 38: group.rotation.x -= rotate; e.preventDefault(); break; // up
+            case 39: group.rotation.y += rotate; e.preventDefault(); break; // left
+            case 40: group.rotation.x += rotate; e.preventDefault(); break; // down
+
+            case 68: camera.position.x += 1; e.preventDefault(); break; // d
+            case 87: camera.position.z -= 1; e.preventDefault(); break; // w
+            case 65: camera.position.x -= 1; e.preventDefault(); break; // a
+            case 83: camera.position.z += 1; e.preventDefault(); break; // s
+        }
+    };
+}(this.document))
